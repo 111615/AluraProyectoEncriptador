dbaf7e3c7f5458757b9b2aa029ce3ae35346fece
Ana Maria <anasarmiento.n@gmail.com>
2023-05-25 20:35:49 -0500

    Modificaciòn boton

--- script.js
+++ script.js
@@ -5,44 +5,44 @@
 // u - ufat 
 
 function encriptar() { 
- var texto = document.getElementById("inputTexto").value.tolowerCase();
+ var texto = document.getElementById("inputTexto").value;
 
 //i es para que afecte tanto mayusculas como minusculas  e E
 //g es para toda linea u oracion     
 //m es para a multiples lneas o parrafos
-var txtCifrado = texto.replace(/e/img,"enter");
-var txtCifrado = txtCifrado.replace(/o/img,"ober");
-var txtCifrado = txtCifrado.replace(/i/img,"imes");
-var txtCifrado = txtCifrado.replace(/a/img,"ai");
-var txtCifrado = txtCifrado.replace(/u/img,"ufat");
+var txtCifrado = texto.replace(/e/gi,"enter");
+var txtCifrado = txtCifrado.replace(/o/gi,"ober");
+var txtCifrado = txtCifrado.replace(/i/gi,"imes");
+var txtCifrado = txtCifrado.replace(/a/gi,"ai");
+var txtCifrado = txtCifrado.replace(/u/gi,"ufat");
 
-document.getElementById("imgDer").style.display = "none";
+//document.getElementById("imgDer").style.display = "none";
 document.getElementById("texto").style.display = "none";
 document.getElementById("texto2").innerHTML = txtCifrado;
-document.getElementById("copiar").style.display = show;
-document.getElementById("texto2").style.display = inherit;
+//document.getElementById("copiar").style.display = show;
+//document.getElementById("texto2").style.display = inherit;
 }
 
 function desencriptar() { 
-    var texto = document.getElementById("inputTexto").value.tolowerCase();
+    var texto = document.getElementById("inputTexto").value;
    //i es para que afecte tanto mayusculas como minusculas  e E
    //g es para toda linea u oracion     
    //m es para a multiples lneas o parrafos
-   var txtCifrado = texto.replace(/enter/img,"e")
-   var txtCifrado = txtCifrado.replace(/ober/img,"o")
-   var txtCifrado = txtCifrado.replace(/imes/img,"i")
-   var txtCifrado = txtCifrado.replace(/ai/img,"a")
-   var txtCifrado = txtCifrado.replace(/ufat/img,"u")
+   var txtCifrado = texto.replace(/enter/gi,"e")
+   var txtCifrado = txtCifrado.replace(/ober/gi,"o")
+   var txtCifrado = txtCifrado.replace(/imes/gi,"i")
+   var txtCifrado = txtCifrado.replace(/ai/gi,"a")
+   var txtCifrado = txtCifrado.replace(/ufat/gi,"u")
    
-   document.getElementById("imgDer").style.display = "none";
+ //  document.getElementById("imgDer").style.display = "none";
    document.getElementById("texto").style.display = "none";
    document.getElementById("texto2").innerHTML = txtCifrado;
-   document.getElementById("copiar").style.display = show;
-   document.getElementById("texto2").style.display = inherit;
+   //document.getElementById("copiar").style.display = show;
+   //document.getElementById("texto2").style.display = inherit;
    }
    function copy() {
-    var contenido = document.querySelector("#texto2");
+    var contenido = document.getElementById("texto2");
     contenido.select();
-    document.execComand(copy);
+    document.execCommand("copy");
     alert("¡Se copio!");
    }
